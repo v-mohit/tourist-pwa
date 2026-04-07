43873cb831f8deb94c3ef5b33efaf21170daf896
v-arshad.ansari <v-arshad.ansari2@gmail.com>
2026-04-07 12:59:27 +0530

    redux removed from project

--- configs/axios.config.ts
+++ configs/axios.config.ts
@@ -1,8 +1,6 @@
 import { getAccessToken, handleLogout } from "@/utils/common.utils";
 import axios from "axios";
 import { NEXT_PUBLIC_API_BASE_URL } from "@/configs/env.config";
-import store from "@/utils/redux/store";
-import { setIsUnauthorized } from "@/utils/redux/slice/globalSlice";
 
 const axiosInstance = axios.create({
   baseURL: NEXT_PUBLIC_API_BASE_URL,
@@ -10,12 +8,10 @@ const axiosInstance = axios.create({
 axiosInstance.defaults.headers.post["Content-Type"] = "application/json";
 
 axiosInstance.interceptors.request.use(async(config) => {
-//   if (config.url !== "/cms-authenticate") {
     const jwt = await getAccessToken();
     if (jwt) {
       config.headers.Authorization = `Bearer ${jwt}`;
     }
-  // }
   return config;
 });
 
@@ -24,23 +20,9 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   (error) => {
-    if(error.response.status === 401){
-      store.dispatch(setIsUnauthorized(true))
-    // showErrorToastMessage("You are not authorised for this operation. Please login to continue.")
-    // handleLogout()
-    // Router.push("/")
+    if (error.response?.status === 401) {
+      handleLogout();
     }
-      if (!navigator.onLine) {
-        // toast.error("You are currently offline", {
-        //   position: toast.POSITION.BOTTOM_RIGHT,
-        // });
-        return Promise.reject(error);
-      } else {
-        // toast.error("APIs not working at the moment. Pleas try again later.", {
-        //   position: toast.POSITION.BOTTOM_RIGHT,
-        // });
-        return Promise.reject(error);
-      }
     return Promise.reject(error);
   }
 );
